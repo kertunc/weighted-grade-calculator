6133adcbf65a8492dc7fa29fbabe8867d6f5f613
kertunc <ertunckilic0@gmail.com>
2025-06-01 02:13:25 +0300

    Add files via upload

---
+++ weighted_grade_calculator.cpp
@@ -0,0 +1,36 @@
+//Kullanicidan 3 sinav notu alip, vizenin %30, projenin %20, finalin %50'sine gore notlarin ortalamasini hesaplayip, gecip gecmedigini yazdiran program.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   float vize , proje , final , ortalama;
+
+   cout<<"Ogrencinin vize notu:";
+   cin>>vize;
+
+   cout<<"Ogrencinin proje notu:";
+   cin>>proje;
+
+   cout<<"Ogrencinin final notu:";
+   cin>>final;
+
+   const int gecmeNotu = 60;
+
+   ortalama = (vize * 0.3) + (proje * 0.2) + (final * 0.5);
+
+   if (ortalama < gecmeNotu)
+   {
+    cout<<"Ogrenci dersten gecemez.";
+   }
+	  else
+		 {
+		  cout<<"Ogrenci dersten gecer.";
+		 }
+
+   cin.ignore();
+   cin.get();
+	return 0;
+}
